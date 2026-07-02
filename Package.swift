50903be700acab4620891e76a8dd4d318ccd4877
Benjamin Williams <benwilliams0540@gmail.com>
2026-07-01 21:03:44 -0400

    build(brushkit): resolve FFI binary from release asset

--- Package.swift
+++ Package.swift
@@ -27,7 +27,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "BrushKitFFI",
-      path: "artifacts/BrushKitFFI.xcframework"
+      url: "https://github.com/benwilliams0540/BrushKit/releases/download/brushkit-ffi-v0.1.0/BrushKitFFI.xcframework.zip",
+      checksum: "b28ae45d8d2aa05dc1eae8d5c34c8ffc1626c5825b9d0604c7ccc141a7d0ad9c"
     )
   ]
 )
