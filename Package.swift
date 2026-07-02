2c92bc3a308df187f622e0247eff7b70b3222043
Benjamin Williams <benwilliams0540@gmail.com>
2026-07-02 02:11:55 -0400

    feat(brushkit): package iOS FFI slices

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
   name: "BrushKit",
   platforms: [
-    .macOS(.v13)
+    .macOS(.v13),
+    .iOS(.v18)
   ],
   products: [
     .library(name: "BrushKit", targets: ["BrushKit"])
@@ -27,8 +28,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "BrushKitFFI",
-      url: "https://github.com/benwilliams0540/BrushKit/releases/download/brushkit-ffi-v0.1.0/BrushKitFFI.xcframework.zip",
-      checksum: "b28ae45d8d2aa05dc1eae8d5c34c8ffc1626c5825b9d0604c7ccc141a7d0ad9c"
+      url: "https://github.com/benwilliams0540/BrushKit/releases/download/brushkit-ffi-v0.2.0/BrushKitFFI.xcframework.zip",
+      checksum: "a751e21a360e3a762d38993cacb0342346454d82c9941ceb7ab9c89d27512b1f"
     )
   ]
 )
